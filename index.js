8cc0a706f5d0fe7a9392a1f23e6cdd5967ecd597
alonbofficial <55811049+alonbofficial@users.noreply.github.com>
2022-09-15 15:41:22 +0300

    Node.js and Express

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const http = require('http')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
 
 let notes = [  
     {    
@@ -20,12 +23,64 @@ let notes = [
         important: true  
     }
 ]
-const app = http.createServer((request, response) => {  
-    //The application/json value in the Content-Type header informs the receiver that the data is in the JSON format:
-    response.writeHead(200, { 'Content-Type': 'application/json' })  
-    //The notes array gets transformed into JSON with the JSON.stringify(notes) method
-    response.end(JSON.stringify(notes))})
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+  
+})
+  
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    
+    if (note) {    
+        response.json(note)  
+    } else {    
+        response.status(404).end()  
+    }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+    
+    response.status(204).end()    
+})
+
+const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => n.id))
+      : 0
+    return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+  
+    if (!body.content) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+  
+    const note = {
+      content: body.content,
+      important: body.important || false,
+      date: new Date(),
+      id: generateId(),
+    }
+  
+    notes = notes.concat(note)
+  
+    response.json(note)
+})
+
 
 const PORT = 3001
-app.listen(PORT)
+app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`)
+})
